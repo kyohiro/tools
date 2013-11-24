e447cc562a1c23c78a379c11c7b95d7c4ea44656
kyohiro <kyohiro@gmail.com>
2013-11-24 22:57:01 +0800

    Heap's permutation

--- permutation/Permutation.scala
+++ permutation/Permutation.scala
@@ -4,15 +4,27 @@ object Permutation {
   //If permutation(seq, N-1) can generate all permutations, by swaping N and previous N-1 elements
   //There'd be N*permutation(seq, N-1) permutations, which results in N! in total
   def backTrackPermutation[T](seq: Array[T]) = {
-      def permutation0(seq: Array[T], boundIndex: Int): Unit = {
-        if (boundIndex == 0) println(seq.mkString)
-        for (i <- 0 to boundIndex) {
-            swap(seq, i, boundIndex)
-            permutation0(seq, boundIndex-1)
-            swap(seq, i, boundIndex)
-        }
+    def permutation0(seq: Array[T], boundIndex: Int): Unit = {
+      if (boundIndex == 0) println(seq.mkString)
+      for (i <- 0 to boundIndex) {
+        swap(seq, i, boundIndex)
+        permutation0(seq, boundIndex-1)
+        swap(seq, i, boundIndex)
       }
-      permutation0(seq, seq.length-1)
+    }
+    permutation0(seq, seq.length-1)
+  }
+  
+  //Heap's permutation algorithm
+  def heapPermutation[T](seq: Array[T]) = {
+    def permutation0(seq: Array[T], boundIndex: Int): Unit = {
+      if (boundIndex == 0) println(seq.mkString)
+      for (i <- 0 to boundIndex) {
+        permutation0(seq, boundIndex-1)
+        swap(seq, boundIndex % 2 * i, boundIndex)
+      }
+    }
+    permutation0(seq, seq.length-1)
   }
   
   def swap[T](seq: Array[T], a: Int, b: Int) = {
